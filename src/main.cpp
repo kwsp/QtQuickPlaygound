0ac66e30e2d1d13a24e059cd1a28b59df6d7243c
Taylor Nie <tnie@tuta.io>
2026-03-26 17:50:34 +0800

    Use configure_file to inject project name into main.cpp
    Replaces the hardcoded "QtQuickPlayground" string in loadFromModule
    with APP_MODULE_NAME, defined via config.h.in from CMAKE_PROJECT_NAME.
    This ensures renaming the project() in CMakeLists is sufficient when
    using this as a template. Also guards the qmake find_program against
    the NOT_FOUND case before calling execute_process.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+#include "config.h"
+
 int main(int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    engine.loadFromModule("QtQuickPlayground", "Main");
+    engine.loadFromModule(APP_MODULE_NAME, "Main");
 
     return QGuiApplication::exec();
 }
