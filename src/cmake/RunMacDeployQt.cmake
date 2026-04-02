ccd5e0ea6a779f57abb5b238ee4dd47c8ec1b793
Taylor Nie <tnie@tuta.io>
2026-04-03 03:12:39 +0800

    fix macos release build

--- src/cmake/RunMacDeployQt.cmake
+++ src/cmake/RunMacDeployQt.cmake
@@ -1,15 +1,18 @@
-if(NOT CONFIG STREQUAL "Release")
-  message(STATUS "Skipping macdeployqt for ${CONFIG} build")
-  return()
+if(CONFIG STREQUAL "Release")
+  # Bundle Qt frameworks/plugins into the .app and produce a .dmg
+  execute_process(
+    COMMAND "${MACDEPLOYQT}" "${BUNDLE_DIR}"
+      -qmldir=${QMLDIR}
+      -verbose=1
+      -codesign=-
+      -no-strip
+      -dmg
+  )
+else()
+  # Write a qt.conf that points to the system Qt installation so the app
+  # can find platform plugins and QML modules without macdeployqt.
+  file(WRITE "${BUNDLE_DIR}/Contents/Resources/qt.conf"
+    "[Paths]\nPlugins = ${QT_PLUGINS_DIR}\nQml2Imports = ${QT_QML_DIR}\n"
+  )
+  message(STATUS "Wrote qt.conf for ${CONFIG} build")
 endif()
-
-execute_process(
-  COMMAND "${MACDEPLOYQT}" "${BUNDLE_DIR}"
-    -qmldir=${QMLDIR}
-    -verbose=1
-    -always-overwrite
-    -codesign=-
-    -no-strip
-    -dmg
-  COMMAND_ERROR_IS_FATAL ANY
-)
